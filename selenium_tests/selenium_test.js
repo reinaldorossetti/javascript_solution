f53a405f94bb8ff2a32c5e9106436c50a1c21a96
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-01-20 15:31:17 -0200

    Update selenium_test.js

--- selenium_tests/selenium_test.js
+++ selenium_tests/selenium_test.js
@@ -1,9 +1,8 @@
 const {Builder, By, Key, until} = require('selenium-webdriver')
-const d = await new Builder().forBrowser('chrome').build();
+const d = new Builder().forBrowser('chrome').build(); d.get('http://www.google.com/ncr');
 (async function google_search() {
-  try {
-    await d.get('http://www.google.com/ncr');
     await d.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await d.wait(until.titleIs('webdriver - Search'), 5000);
-  } finally { await d.close();}
+    await d.wait(until.titleIs('webdriver - Google Search'), 5000); // Nao precisa de assert, se nao encontra vai dar erro.
+}
+d.close();
 })();
